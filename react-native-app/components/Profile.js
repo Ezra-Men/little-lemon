1ac3c1ec8139ee13ccad58eaa6d1d0f127106fad
Ezra Men Joseph <ezramen2@outlook.com>
2024-09-27 09:31:33 +0100

    new updates

--- react-native-app/components/Profile.js
+++ react-native-app/components/Profile.js
@@ -62,6 +62,7 @@ const ProfileScreen = ({ navigation }) => {
   return (
     <ScrollView>
       <View style={{ flex: 1, padding: 20 }}>
+        <Text style={{fontSize:20}}>Personal Information</Text>
         <TouchableOpacity onPress={handleImagePicker}>
           {image ? (
             <Image source={{ uri: image }} style={{ width: 100, height: 100, borderRadius: 50 }} />
@@ -108,14 +109,15 @@ const ProfileScreen = ({ navigation }) => {
           onPress={() => setOffersCheckbox(!offersCheckbox)}
           title="Receive special offers"
         />
+        <View style={{flexDirection: 'row', padding: 10, margin: 15, justifyContent: 'space-between'}}>
+          <TouchableOpacity onPress={handleSaveChanges} style={{ backgroundColor: '#F4CE14', padding: 10, borderRadius: 10, width: 130, height: 40, alignItems: 'center', marginTop: 20 }}>
+            <Text style={{ color: '#000000' }}>Save Changes</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={handleSaveChanges} style={{ backgroundColor: '#333', padding: 10, borderRadius: 5, width: 100, alignItems: 'center', marginTop: 20 }}>
-          <Text style={{ color: '#fff' }}>Save Changes</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={handleLogout} style={{ backgroundColor: '#333', padding: 10, borderRadius: 10, width: 100, alignItems: 'center', marginTop: 20 }}>
-          <Text style={{ color: '#fff' }}>Logout</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={handleLogout} style={{ backgroundColor: '#F4CE14', padding: 10, borderRadius: 10, width: 130, height:40, alignItems: 'center', marginTop: 20 }}>
+            <Text style={{ color: '#000000' }}>Logout</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
